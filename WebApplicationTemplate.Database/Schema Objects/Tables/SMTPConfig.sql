f0126bb3d5d55573e7cbcc4c2b0a98b4a20e29fd
Erik <erik.cast@hotmail.com>
2017-04-22 11:28:20 -0500

    Archivo que faltaba..

---
+++ WebApplicationTemplate.Database/Schema Objects/Tables/SMTPConfig.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SMTPConfig]
+(
+	[IdSMTPConfig] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Server] VARCHAR(255) NOT NULL, 
+    [Port] INT NOT NULL, 
+    [User] VARCHAR(100) NOT NULL, 
+    [Password] VARCHAR(100) NOT NULL
+)
