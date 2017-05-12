6caec0e21bc8905f1b5bab18a0d0ef2c4958965f
Erik <erik.cast@hotmail.com>
2017-05-12 11:35:00 -0500

    Corrección en la obtención de las Urls

--- WebApplicationTemplate.Web/Tools/Urls.cs
+++ WebApplicationTemplate.Web/Tools/Urls.cs
@@ -11,8 +11,8 @@ namespace WebApplicationTemplate.Web.Tools
 		public static String Abs(String url)
 		{
             //StringBuilder abs = new StringBuilder("http://setgosrv-001-site1.ftempurl.com");
-            StringBuilder abs = new StringBuilder(HttpContext.Current.Request.ApplicationPath);
-            //StringBuilder abs = new StringBuilder(WebSettings.URLApplicationPath);
+            //StringBuilder abs = new StringBuilder(HttpContext.Current.Request.ApplicationPath);
+            StringBuilder abs = new StringBuilder(WebSettings.URLApplicationPath);
 
             if (url.StartsWith("/"))
 			{
